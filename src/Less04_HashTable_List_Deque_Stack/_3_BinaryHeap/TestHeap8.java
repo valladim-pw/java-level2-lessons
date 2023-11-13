a9521d81a6be6b451e48051ae186969b2e460be0
Vadim <vkodoch@gmail.com>
2023-11-13 21:36:34 +0300

    change tests

--- src/Less04_HashTable_List_Deque_Stack/_3_BinaryHeap/TestHeap8.java
+++ src/Less04_HashTable_List_Deque_Stack/_3_BinaryHeap/TestHeap8.java
@@ -11,19 +11,21 @@ public class TestHeap8 {
 	
 	public static void main(String[] args) {
 		BinaryHeapFull2<Integer> heap = new BinaryHeapFull2<>(BinaryHeapFull2.Type.MIN_HEAP);
-//		for(int i = 10000; i >= 9990; i--) {
-//			heap.add(i);
-//		}
+		for(int i = 10000; i >= 9990; i--) {
+			heap.add(i);
+		}
+		
 		System.out.println("-------------------------------------");
 		String str = "БУМАГОПРЯДИЛЬНЫЙ";
 		String[] arrStr = str.split("");
-		//System.out.println(Arrays.toString(arrStr));
 		BinaryHeapFull2<String> heapS = new BinaryHeapFull2<>();
-		//heapS.from(BinaryHeapFull2.Type.MIN_HEAP, arrStr);
+		heapS.from(BinaryHeapFull2.Type.MIN_HEAP, arrStr);
+		
 		System.out.println("-------------------------------------");
 		String str2 = "Replaces each substring of this string that matches the given regular expression";
 		String[] arrStr2 = str2.split(" ");
 		heapS.from(BinaryHeapFull2.Type.MAX_HEAP, arrStr2);
+		
 		System.out.println("-------------------------------------");
 		String str3 = "Общая длина 90-миллиардного мегаинфраструктурного проекта Дели – Мумбай составляет 1483 км";
 		String[] arrStr3 = str3.split(" ");
