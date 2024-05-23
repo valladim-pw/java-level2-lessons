f20856e86c357239e28dd337eaae6243fe8eaa90
Vadim <vkodoch@gmail.com>
2024-05-23 14:45:37 +0300

    maven step24

---
+++ src/Less06_BuildingMavenParentPOM/Test.java
@@ -0,0 +1,12 @@
+package Less06_BuildingMavenParentPOM;
+
+import java.util.Date;
+import java.time.Instant;
+import java.util.Calendar;
+
+public class Test {
+	public static void main(String[] args) {
+		String time = "" + new Date().toString();
+		System.out.println(time);
+	}
+}
--- src/Less06_MavenPlugin/MavenPlugin/src/main/java/Less06_MavenPlugin/MyMojo.java
+++ src/Less06_MavenPlugin/MavenPlugin/src/main/java/Less06_MavenPlugin/MyMojo.java
@@ -8,56 +8,55 @@ import org.apache.maven.plugins.annotations.LifecyclePhase;
 import org.apache.maven.plugins.annotations.Mojo;
 import org.apache.maven.plugins.annotations.Parameter;
 
-import java.io.File;
-import java.io.FileWriter;
-import java.io.IOException;
+import java.io.*;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+
 
 /**
  * Goal which touches a timestamp file.
  */
 @Mojo( name = "touch", defaultPhase = LifecyclePhase.PROCESS_SOURCES )
-public class MyMojo
-    extends AbstractMojo
-{
+public class MyMojo extends AbstractMojo {
+    
     /**
      * Location of the file.
      */
     @Parameter( defaultValue = "${project.build.directory}", property = "outputDir", required = true )
     private File outputDirectory;
-
-    public void execute()
-        throws MojoExecutionException
-    {
+    List<String> list;
+    
+    public void execute() throws MojoExecutionException {
         File f = outputDirectory;
 
-        if ( !f.exists() )
-        {
+        if (!f.exists()) {
             f.mkdirs();
         }
-
-        File touch = new File( f, "touch.txt" );
+        File touch = new File(f,"touch.txt");
 
         FileWriter w = null;
-        try
-        {
-            w = new FileWriter( touch );
-
-            w.write( "touch.txt" );
-        }
-        catch ( IOException e )
-        {
+        Path path = Paths.get(String.valueOf(touch));
+        try {
+            w = new FileWriter(touch, true);
+            list = Files.readAllLines(path);
+            int count = list.size();
+            Date date = new Date();
+            
+            String time = (count + 1) + ": " + date.toString() + "\n";
+            w.write(time);
+        } catch (IOException e) {
+            
             throw new MojoExecutionException( "Error creating file " + touch, e );
-        }
-        finally
-        {
-            if ( w != null )
-            {
-                try
-                {
+            
+        } finally {
+            if (w != null) {
+                try {
                     w.close();
-                }
-                catch ( IOException e )
-                {
+                } catch (IOException e) {
                     // ignore
                 }
             }
