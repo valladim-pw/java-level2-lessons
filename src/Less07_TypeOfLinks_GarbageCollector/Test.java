2045eb7967841585075dcc04068475bb6d3570ad
Vadim <vkodoch@gmail.com>
2024-07-29 22:26:57 +0300

    changes 29.07.24

---
+++ src/Less07_TypeOfLinks_GarbageCollector/Test.java
@@ -0,0 +1,4 @@
+package Less07_TypeOfLinks_GarbageCollector;
+
+public class Test {
+}
---
+++ src/Less07_TypeOfLinks_GarbageCollector/_1_GarbageCollector/FiboWithRecursion.java
@@ -0,0 +1,35 @@
+package Less07_TypeOfLinks_GarbageCollector._1_GarbageCollector;
+
+public class FiboWithRecursion {
+	int prev;
+	int current;
+	public FiboWithRecursion(int prev, int current) {
+		this.prev = prev;
+		this.current = current;
+	}
+	
+	FiboWithRecursion next() {
+		return new FiboWithRecursion(current, prev + current);
+	}
+	
+	@Override
+	public String toString() {
+		return String.valueOf(current);
+	}
+	
+	public static FiboWithRecursion getFibo(int num) {
+		switch (num) {
+			case 0: return new FiboWithRecursion(0, 1);
+			case 1: return new FiboWithRecursion(1, 1);
+		}
+		FiboWithRecursion preFibo = getFibo(num - 1);
+		return preFibo.next();
+	}
+	
+	public static void main(String[] args) {
+		for (int i = 0; i < 10; i++) {
+			FiboWithRecursion fwr = FiboWithRecursion.getFibo(i);
+			System.out.println(fwr);
+		}
+	}
+}
